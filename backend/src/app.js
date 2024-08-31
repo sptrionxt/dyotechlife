3380b06f0280a641b64c1b45985b28df232ca33a
Shubham Patil <shubham.patil@trionxt.com>
2024-08-31 19:38:27 +0530

    cors update

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(cookieParser()); 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 
 // Route imports
 
