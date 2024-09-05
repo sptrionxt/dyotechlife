52fea2c36721809b142b7c9fc5bf0df66a772f63
Shubham Patil <shubham.patil@trionxt.com>
2024-09-05 12:02:11 +0530

    DB changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connectDatabase = async() => {
-    try {
-        await mongoose.connect(process.env.DB_URI)
-        console.log("mongodb connected with server");
-    }
-     catch (error) {
-        throw error
-    }
-    
-}
-
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+      console.log("mongodb connected with server");
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = connectDatabase;
